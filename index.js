96a077eb05a86ca2858e5d734740f1b39a4be93a
VibewithZari <azarieac@knights.ucf.edu>
2023-02-09 12:18:30 -0500

    Created quote app

---
+++ index.js
@@ -0,0 +1,3 @@
+let peopleImportant=["Estarías enojado con el mundo cuando experimentas una desgracia, pero es importante recordar exactamente por qué estás enojado.", "You do not realize you are in the moment, until it is a memory.", "Great things are done by a series of small things brought together"]
+console.log(Math.random())
+console.log(peopleImportant[0])
